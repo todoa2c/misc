a8165d9e74b969a2f6a2b19a782c4c896d46ccff
Atsushi Kanaya <todoa2c@gmail.com>
2012-07-25 14:22:29 +0900

    removed duprecated patterns

--- bossgame/bossctrl.js
+++ bossgame/bossctrl.js
@@ -58,10 +58,15 @@ function BossCtrl($scope) {
          hideEdge();
          $scope.count++;
          console.log('clicked row: ' + row + ', col: ' + col);
-    }
+    };
+
+    var sortByOrder = function(a, b) {
+        return a - b;
+    };
 
     $scope.solve = function() {
         var pattern = 0;
+        var start = new Date();
         while(true) {
             var patList = [];
             var temp = pattern;
@@ -71,20 +76,38 @@ function BossCtrl($scope) {
                 temp = ((temp - mod) / 16) - 1;
             }
             patList.push(temp);
-            var copy = angular.copy(matrix);
-            var result = trySolve(copy, patList);
-            if (result) {
-                console.log(pattern + ' : ' + JSON.stringify(patList) + ' => ' + result);
-                $scope.pattern = [];
-                for (var i = 0; i < patList.length; i++) {
-                    $scope.pattern = patList.sort();
+
+            if (!hasDuplicatedPattern(patList)) {
+                var copy = angular.copy(matrix);
+                var result = trySolve(copy, patList);
+                if (result) {
+                    console.log(pattern + ' : ' + JSON.stringify(patList) + ' => ' + result);
+                    $scope.pattern = [];
+                    for (var i = 0; i < patList.length; i++) {
+                        $scope.pattern = patList.sort(sortByOrder);
+                    }
+                    var stop = new Date();
+                    console.log('time = ' + (stop.getTime() - start.getTime()) + ' [ms]');
+                    $scope.indexState = 'show';
+                    break;
                 }
-                $scope.indexState = 'show';
-                break;
+            } else {
+                //console.log('DUPLICATED: ' + pattern + ' : ' + JSON.stringify(patList));
             }
             pattern++;
         }
-    }
+    };
+
+    var hasDuplicatedPattern = function(patList) {
+        var map = {};
+        for (var i = 0; i < patList.length; i++) {
+            if (patList[i] in map) {
+                return true;
+            }
+            map[patList[i]] = patList[i];
+        }
+        return false;
+    };
 
     var trySolve = function(matrix, patList) {
         for (var i = 0; i < patList.length; i++) {
